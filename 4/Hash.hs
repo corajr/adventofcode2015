6ddd4fb8afd1e302976fa878214139dec59f6c51
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-06 18:34:45 -0500

    pureMD5 is way faster

--- 4/Hash.hs
+++ 4/Hash.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Hash where
+module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.List (find)
 import Data.Digest.Pure.MD5
 
-hashStartsWithZeros x = take 5 (show (md5 x)) == "00000"
+zeros n = replicate n '0'
+
+hashStartsWithZeros n x = take n (show (md5 x)) == zeros n
 
 input = "yzbqklnj"
 
-lst = map (\(x,y) -> x `B.append` B.pack (show y)) (zip (repeat input) [1..])
+lst n = map (\(x,y) -> x `B.append` B.pack (show y)) (zip (repeat input) [n..])
 
-main = print $ find hashStartsWithZeros lst
+partOne = print $ find (hashStartsWithZeros 5) (lst 1)
+
+partTwo = print $ find (hashStartsWithZeros 6) (lst 282749)
+
+main = partOne
