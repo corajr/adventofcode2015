0643c8be6a99842dbc5732d2a5a6b8d9189719bd
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-06 21:16:37 -0500

    merge; start 5

---
+++ 5/Nice.hs
@@ -0,0 +1,19 @@
+module Nice where
+
+import Text.Regex.TDFA
+
+vowels :: String -> Bool
+vowels x = (x =~ "[aeiou]" :: Int) >= 3
+
+noBadStrings :: String -> Bool
+noBadStrings x = not (x =~ "(ab|cd|pq|xy)")
+
+doubled :: String -> Bool
+doubled x = x =~ "(.)\\1"
+
+nice :: String -> Bool
+nice x = noBadStrings x && vowels x && doubled x
+
+main = do
+  input <- readFile "input.txt"
+  print $ length $ filter nice (lines input)
