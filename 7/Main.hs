2d275d2709edb3a67a9658992cd5823d6b2e1d5c
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-07 22:58:22 -0500

    7.2

--- 7/Main.hs
+++ 7/Main.hs
@@ -1,15 +1,20 @@
 module Main where
 
 import Circuit
-import Solve
+import Solve (solve)
 import qualified Data.Map.Strict as Map
+import Data.List (find)
 import Text.ParserCombinators.Parsec (parseFromFile)
 
-partOne circ =
-  case circ of
-    Left err -> fail (show err)
-    Right circ' -> print $ Map.lookup "a" (solve circ')
+partOne circ = Map.lookup "a" (solve circ)
+
+partTwo circ = Map.lookup "a" (solve circ')
+  where b = find (\(Connection _ w) -> w == Wire "b") circ
+        replaceB = Connection (V (Lit 16076)) (Wire "b")
+        circ' = map (\x -> if Just x == b then replaceB else x) circ
 
 main = do
   circ <- parseFromFile circuit "input.txt"
-  partOne circ
+  case circ of
+    Left err -> fail (show err)
+    Right circ' -> print $ partTwo circ'
