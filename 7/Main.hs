efe7593f7ac978737e385f77886fb59d4fbbbd51
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-07 22:48:31 -0500

    7.1

--- 7/Main.hs
+++ 7/Main.hs
@@ -1,3 +1,15 @@
 module Main where
 
-main = print ""
+import Circuit
+import Solve
+import qualified Data.Map.Strict as Map
+import Text.ParserCombinators.Parsec (parseFromFile)
+
+partOne circ =
+  case circ of
+    Left err -> fail (show err)
+    Right circ' -> print $ Map.lookup "a" (solve circ')
+
+main = do
+  circ <- parseFromFile circuit "input.txt"
+  partOne circ
