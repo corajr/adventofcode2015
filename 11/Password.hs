0328d1c985cf1f5fd0db9354f8b0cc2405a59c39
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-13 13:03:03 -0500

    criteria pass

--- 11/Password.hs
+++ 11/Password.hs
@@ -1,6 +1,8 @@
 module Password where
 
 import Text.Regex.PCRE
+import Data.Char (ord, chr)
+import Data.List (intercalate)
 
 inc :: String -> String
 inc = undefined
@@ -8,8 +10,14 @@ inc = undefined
 requirements :: String -> Bool
 requirements inp = all ($ inp) [straight, unambiguous, twoPairs]
 
-straight = undefined
+triples = "(" ++ intercalate "|" (map makeTriple [ord 'a' .. ord 'x']) ++ ")"
+  where makeTriple a = [chr a, chr (a + 1), chr (a + 2)]
 
-unambiguous = undefined
+straight :: String -> Bool
+straight inp = inp =~ triples
 
-twoPairs = undefined
+unambiguous :: String -> Bool
+unambiguous inp = not (inp =~ "(i|o|l)")
+
+twoPairs :: String -> Bool
+twoPairs inp = inp =~ "(.)\\1.*(.)\\2"
