116eff9aaa22d07d3f333fa79f8bd89af3441a41
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-13 15:42:46 -0500

    make array

--- 13/Seating.hs
+++ 13/Seating.hs
@@ -2,6 +2,8 @@ module Seating where
 
 import Text.Regex.PCRE
 import Data.Array.Unboxed
+import Data.List (nub, elemIndex)
+import Data.Maybe (fromMaybe)
 
 type Person = String
 
@@ -14,6 +16,7 @@ data Datum = Datum
 data HappyInfo = HappyInfo [Person] ValueMatrix
           deriving (Show, Eq)
 
+type IEdge = ((Int, Int), Int)
 type ValueMatrix = UArray (Int, Int) Int
 
 factRegex = "(\\w+) would (gain|lose) (\\d+) happiness units by sitting next to (\\w+)."
@@ -24,11 +27,29 @@ parseFact l =
       mult = if g == "gain" then 1 else -1
   in Datum s (mult * (read c)) o
 
-factsToInfo :: [Datum] -> HappyInfo
-factsToInfo = undefined
 
 parseValues :: String -> HappyInfo
 parseValues = factsToInfo . map parseFact . lines
+
+getPeople = nub . concatMap (\(Datum p1 _ p2) -> [p1, p2])
+
+edgesToIEdges people edges =
+  let lookUp p = fromMaybe (-1) (elemIndex p people)
+      f (Datum p1 v p2) = ((lookUp p1, lookUp p2), v)
+  in map f edges
+
+factsToInfo :: [Datum] -> HappyInfo
+factsToInfo edges =
+  let verts = getPeople edges
+      n = length verts
+      iEdges = edgesToIEdges verts edges
+      values = array ((0,0), (n - 1,n - 1)) iEdges
+  in HappyInfo verts values
+
+window :: Int -> [a] -> [[a]]
+window n lst@(_:xs)
+  | length lst < n = []
+  | otherwise = take n lst : window n xs
 
 maxValue :: HappyInfo -> Int
 maxValue _ = 0
--- 13/Spec.hs
+++ 13/Spec.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Test.Hspec
 import Seating
+import Data.Array.Unboxed
 
 sample1 = "Alice would gain 54 happiness units by sitting next to Bob."
 sample2 = "Alice would lose 79 happiness units by sitting next to Carol."
@@ -21,6 +22,9 @@ sampleData =
   , Datum {subject = "David", change = 41, object = "Carol"}
   ]
 
+sampleInfo = HappyInfo ["Alice", "Bob", "Carol", "David"]
+             (array ((0,0),(3,3)) [((0,0),0),((0,1),54),((0,2),-79),((0,3),-2),((1,0),83),((1,1),0),((1,2),-7),((1,3),-63),((2,0),-62),((2,1),60),((2,2),0),((2,3),55),((3,0),46),((3,1),-7),((3,2),41),((3,3),0)])
+
 main = do
   test_input <- readFile "test_input.txt"
   hspec $ do
@@ -30,6 +34,9 @@ main = do
         parseFact sample2 `shouldBe` Datum "Alice" (-79) "Carol"
       it "should, when mapped, parse a sample set of facts" $
         map parseFact (lines test_input) `shouldBe` sampleData
+    describe "factsToInfo" $ do
+      it "should process a sample set of facts" $
+        factsToInfo sampleData `shouldBe` sampleInfo
     describe "maxValue" $
       it "should give 330 for the sample arrangement" $ do
         let vs = parseValues test_input
