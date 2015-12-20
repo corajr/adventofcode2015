ff922053b9d559801863d5a108f684649873428f
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-20 14:13:12 -0500

    shockingly, exponential search isn't the right choice for 19.2

--- 19/src/Medicine.hs
+++ 19/src/Medicine.hs
@@ -7,12 +7,19 @@ import Control.Monad (guard)
 import Medicine.Parser
 
 getDistinctMolecules :: Medicine -> Int
-getDistinctMolecules = Set.size . Set.fromList . getMolecules
+getDistinctMolecules = Set.size . getMolecules
 
-getMolecules :: Medicine -> [Molecule]
-getMolecules (Medicine subst m) = do
+getMolecules :: Medicine -> Set.Set Molecule
+getMolecules (Medicine subst m) = Set.fromList $ do
   Substitute from to <- subst
   (atom, i) <- zip m [0..]
   guard (from == atom)
   let (xs, _:ys) = splitAt i m
   return $ xs ++ to ++ ys
+
+buildInSteps :: Medicine -> Int
+buildInSteps med = go 0 (Set.singleton ["e"])
+  where go i xs
+          | molecule med `Set.member` xs = i
+          | otherwise = go (i + 1) (Set.unions (map update (Set.toList xs)))
+        update x = getMolecules $ med { molecule = x }
--- 19/test/MedicineSpec.hs
+++ 19/test/MedicineSpec.hs
@@ -3,15 +3,36 @@ module MedicineSpec (main, spec) where
 import Test.Hspec
 import Medicine
 import Medicine.ParserSpec (parsedMedicine)
+import Text.ParserCombinators.Parsec (parse)
+
+sampleBuild =
+  unlines [ "e => H"
+          , "e => O"
+          , "H => HO"
+          , "H => OH"
+          , "O => HH"
+          , ""
+          , "HOH"
+          ]
+
+parsedBuild = case parse pMedicine "" sampleBuild of
+  Left err -> error (show err)
+  Right p -> p
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
+spec = do
   describe "getDistinctMolecules" $ do
     it "should return 4 for HOH" $
       getDistinctMolecules parsedMedicine `shouldBe` 4
     it "should return 7 for HOHOHO" $ do
       let med = parsedMedicine { molecule = ["H", "O", "H", "O", "H", "O"]}
       getDistinctMolecules med `shouldBe` 7
+  describe "buildInSteps" $ do
+    it "should take 3 steps to make HOH" $
+      buildInSteps parsedBuild `shouldBe` 3
+    it "should take 6 steps to make HOHOHO" $ do
+      let build = parsedBuild { molecule = ["H", "O", "H", "O", "H", "O"] }
+      buildInSteps build `shouldBe` 6
