60538fc1305fced03c49e577cdd4ac4e9f0c08a7
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-20 16:13:12 -0500

    try working backwards - no dice

--- 19/src/Medicine.hs
+++ 19/src/Medicine.hs
@@ -6,6 +6,7 @@ import qualified Data.HashMap.Strict as Map
 import qualified Data.HashSet as Set
 import Control.Parallel.Strategies
 import Control.Monad (guard)
+import Data.Maybe (isJust)
 import Medicine.Parser
 
 getDistinctMolecules :: Medicine -> Int
@@ -18,9 +19,25 @@ getMolecules med@(Medicine _ m) = Set.fromList $ do
   let (xs, _:ys) = splitAt i m
   return $ xs ++ to ++ ys
 
+getPriorGen :: Medicine -> Set.HashSet Molecule
+getPriorGen med@(Medicine _ m) = Set.fromList $ do
+  let revMap = toRevMap med
+      longest = maximum . map length $ Map.keys revMap
+      n = length m
+  i <- [0..n-1]
+  j <- [1..longest]
+  let substr = take j (drop i m)
+      atom = Map.lookup substr revMap
+  case atom of
+    Just "e" -> if length substr == n
+                   then return ["e"]
+                   else []
+    Just a -> return $ take i m ++ [a] ++ drop (i+j) m
+    Nothing -> []
+
 buildInSteps :: Medicine -> Int
-buildInSteps med = go 0 (Set.singleton ["e"])
+buildInSteps med = go 0 (Set.singleton $ molecule med)
   where go i xs
-          | molecule med `Set.member` xs = i
+          | ["e"] `Set.member` xs = i
           | otherwise = go (i + 1) (Set.unions (parMap rdeepseq update (Set.toList xs)))
-        update x = getMolecules $ med { molecule = x }
+        update x = getPriorGen $ med { molecule = x }
--- 19/test/MedicineSpec.hs
+++ 19/test/MedicineSpec.hs
@@ -1,8 +1,9 @@
-module MedicineSpec (main, spec) where
+module MedicineSpec (main, spec, parsedBuild) where
 
 import Test.Hspec
 import Medicine
 import Medicine.ParserSpec (parsedMedicine)
+import qualified Data.HashSet as Set
 import Text.ParserCombinators.Parsec (parse)
 
 sampleBuild =
@@ -19,17 +20,47 @@ parsedBuild = case parse pMedicine "" sampleBuild of
   Left err -> error (show err)
   Right p -> p
 
+priorGenHOH =
+  Set.fromList [ ["H", "H"]
+               ]
+
+priorGenHOHOHO =
+  Set.fromList [ ["H", "O", "H", "O", "H"]
+               , ["H", "H", "O", "H", "O"]
+               , ["H", "O", "H", "H", "O"]
+               ]
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
+  let med = parsedMedicine { molecule = ["H", "O", "H", "O", "H", "O"]}
   describe "getDistinctMolecules" $ do
     it "should return 4 for HOH" $
       getDistinctMolecules parsedMedicine `shouldBe` 4
-    it "should return 7 for HOHOHO" $ do
-      let med = parsedMedicine { molecule = ["H", "O", "H", "O", "H", "O"]}
+    it "should return 7 for HOHOHO" $
       getDistinctMolecules med `shouldBe` 7
+  describe "getPriorGen" $ do
+    let medH = parsedBuild { molecule = ["H"]}
+    let medO = parsedBuild { molecule = ["O"]}
+    let medHH = parsedBuild { molecule = ["H", "H"]}
+    let medHO = parsedBuild { molecule = ["H", "O"]}
+    let medOH = parsedBuild { molecule = ["O", "H"]}
+    it "should return one step back for H" $
+      getPriorGen medH `shouldBe` Set.fromList [["e"]]
+    it "should return one step back for O" $
+      getPriorGen medO `shouldBe` Set.fromList [["e"]]
+    it "should return one step back for HH" $
+      getPriorGen medHH `shouldBe` Set.fromList [["O"]]
+    it "should return one step back for HO" $
+      getPriorGen medHO `shouldBe` Set.fromList [["H"]]
+    it "should return one step back for OH" $
+      getPriorGen medOH `shouldBe` Set.fromList [["H"]]
+    it "should return one step back for HOH" $
+      getPriorGen parsedMedicine `shouldBe` priorGenHOH
+    it "should return one step back for HOHOHO" $
+      getPriorGen med `shouldBe` priorGenHOHOHO
   describe "buildInSteps" $ do
     it "should take 3 steps to make HOH" $
       buildInSteps parsedBuild `shouldBe` 3
