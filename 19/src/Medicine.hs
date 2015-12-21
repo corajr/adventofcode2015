a7b47db8f7bb905e54bb4da1df3acaba5e3118e8
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-21 11:41:21 -0500

    19.2

--- 19/src/Medicine.hs
+++ 19/src/Medicine.hs
@@ -4,12 +4,13 @@ module Medicine ( module Medicine.Parser
 
 import qualified Data.HashMap.Strict as Map
 import qualified Data.HashSet as Set
-import qualified Data.PQueue.Prio.Min as PQ
-import Control.Parallel.Strategies
-import Data.List (foldl')
-import Text.EditDistance
 import Debug.Trace (trace)
+import System.Random.Shuffle (shuffleM)
+import System.Random (mkStdGen)
+import Control.Monad.Random (evalRand)
 import Medicine.Parser
+
+type Substrings = Map.HashMap Molecule (Int, Int)
 
 getDistinctMolecules :: Medicine -> Int
 getDistinctMolecules = Set.size . getMolecules
@@ -21,32 +22,38 @@ getMolecules med@(Medicine _ m) = Set.fromList $ do
   let (xs, _:ys) = splitAt i m
   return $ xs ++ to ++ ys
 
-start = ["e"]
-
--- copy of https://gist.github.com/abhin4v/8172534
+substrings :: Int -> Molecule -> Substrings
+substrings maxLen mole = Map.fromList $ do
+  let n = length mole
+  i <- [0..n-1]
+  j <- [1.. (minimum [maxLen, n-i])]
+  let substr = take j (drop i mole)
+  return (substr, (i, j))
 
 search :: Medicine -> Maybe Int
-search med =
-  astar (PQ.singleton (heuristic start) (start, 0))
-        Set.empty (Map.singleton start 0)
-  where
-    heuristic x = if length x > length (molecule med) then error "search node longer than real molecule" else textHeuristic x
-    textHeuristic x = levenshteinDistance defaultEditCosts (concat x) (concat $ molecule med)
-    astar pq seen gscore
-      | PQ.null pq = Nothing
-      | node == molecule med = Just gcost
-      | node `Set.member` seen = astar pq' seen gscore
-      | otherwise = astar pq'' seen' gscore'
-      where
-        (node, gcost) = snd . PQ.findMin $ pq
-        pq' = PQ.deleteMin pq
-        seen' = Set.insert node seen
-        successors = filter (\(s, g, _) -> not (s `Set.member` seen') &&
-                                            (g < Map.lookupDefault maxBound s gscore))
-                             successorsAndCosts
-                             -- (trace (show successorsAndCosts) successorsAndCosts)
-        pq'' = foldl' (\q (s, g, h) -> PQ.insert (g + h) (s, g) q) pq' successors
-        gscore' = foldl' (\m (s, g, _) -> Map.insert s g m) gscore successors
-        successorsAndCosts =
-          parMap rdeepseq (\s -> (s, gcost + 1, heuristic s)) . Set.toList . getMolecules $ med {molecule = node}
+search med = evalRand (searchBack med) (mkStdGen 0)
 
+searchBack origin = go 0 [""] (substitutes origin) origin
+  where go i _ _ (Medicine _ ["e"]) = return $ Just i
+        go i prior [] med@(Medicine subst mole)
+          | mole == prior = redoOrder med >>= go i [""] subst
+          | otherwise = go i prior subst med
+        go i prior (x:xs) med@(Medicine subst mole) =
+          let substrs = substrings (maximum . map (length . (\(Substitute _ mol) -> mol)) $ subst) mole
+          in case applyTransform x substrs mole of
+            Just newMole -> go (i + 1) mole xs (med { molecule = newMole } )
+            Nothing -> go i mole xs med
+
+trace' :: Show a => a -> a
+trace' a = trace (show a) a
+
+applyTransform :: Substitute -> Substrings -> Molecule -> Maybe Molecule
+applyTransform (Substitute at mol) substrs mol'
+  | at == "e" = if mol' == mol
+                   then Just ["e"]
+                   else Nothing
+  | otherwise = fmap f (Map.lookup mol substrs)
+  where f (i, j) = take i mol' ++ [at] ++ drop (i+j) mol'
+
+redoOrder (Medicine subst m) =
+  Medicine <$> shuffleM subst <*> pure m
