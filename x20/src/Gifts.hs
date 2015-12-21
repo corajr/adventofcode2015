194e2cd6828fe5f5824ce7fc887e5b10828951ba
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-12-21 12:15:55 -0500

    20.2

--- x20/src/Gifts.hs
+++ x20/src/Gifts.hs
@@ -1,10 +1,14 @@
 module Gifts where
 
 import Data.List (findIndex)
-import Math.NumberTheory.Primes.Factorisation (sigma)
+import qualified Data.Set as Set
+import Math.NumberTheory.Primes.Factorisation (divisors, divisorSum)
 
 houses = [1..]
 
-gifts = map ((*10) . sigma 1) houses
+partOne = divisorSum
+partTwo n = sum . filter (\x -> x*50 >= n) . Set.toList $ divisors n
+
+gifts = map ((*11) . partTwo) houses
 
 findHouse p = fmap (+1) $ findIndex p gifts
